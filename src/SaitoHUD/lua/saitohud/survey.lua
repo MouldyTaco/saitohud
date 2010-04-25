ae92d8fcb855e3c7788ae844f7b2af0d98fbbfaa
the.sk89q <the.sk89q@943c4650-7562-7562-3aaf-4581b7f2fa02>
2010-04-25 03:32:18 +0000

    Added path measuring.

--- src/SaitoHUD/lua/saitohud/survey.lua
+++ src/SaitoHUD/lua/saitohud/survey.lua
@@ -26,6 +26,8 @@ local reflectTraceColorProgression = CreateClientConVar("reflect_trace_color_pro
 
 local orthogonalTraces = {}
 local reflectionLines = {}
+local measurePoints = {}
+local measureTotalLength = 0
 
 local Rehook = nil
 
@@ -98,6 +100,289 @@ local function ReflectAnalysis(ply, cmd, args)
     Rehook()
 end
 
+--- Recalculate the measured total.
+local function RecalcMeasuredTotal()
+    measureTotalLength = 0
+    
+    if #measurePoints > 1 then
+        local last = measurePoints[1]
+        
+        for i = 2, #measurePoints do
+            local pt = measurePoints[i]
+            measureTotalLength = measureTotalLength + pt:Distance(last)
+            last = pt
+        end
+    end
+end
+
+--- Console commands to add a point to the path measurement tool.
+-- @param ply Player
+-- @param cmd Command
+-- @param args Arguments
+local function AddMeasuredPoint(ply, cmd, args)
+    local vec = nil
+    
+    if #args == 1 or #args == 3 then
+        vec = SaitoHUD.ParseConcmdVector(args)
+    elseif #args ~= 0 then
+        Msg("Invalid number of arguments\n")
+        return
+    else
+        local tr = SaitoHUD.GetRefTrace()
+        vec = tr.HitPos
+    end
+    
+    if vec == nil then
+        Msg("Invalid arguments\n")
+        return
+    end
+    
+    if #measurePoints > 0 then
+        local last = measurePoints[#measurePoints]
+        measureTotalLength = measureTotalLength + vec:Distance(last)
+        
+        print("Added point #" .. #measurePoints + 1)
+        print(string.format("Incremental distance: %f",
+                            last:Distance(vec)))
+        print(string.format("Total distance: %f", measureTotalLength))
+    end
+    
+    table.insert(measurePoints, vec)
+    
+    Rehook()
+end
+
+--- Console command to insert a point in the path measurement tool.
+-- @param ply Player
+-- @param cmd Command
+-- @param args Arguments
+local function InsertMeasuredPoint(ply, cmd, args)
+    local vec = nil
+    
+    if #args == 2 or #args == 4 then
+        vec = SaitoHUD.ParseConcmdVector(args, 1)
+    elseif #args ~= 1 then
+        Msg("Invalid number of arguments\n")
+        return
+    else
+        local tr = SaitoHUD.GetRefTrace()
+        vec = tr.HitPos
+    end
+    
+    if vec == nil then
+        Msg("Invalid arguments\n")
+        return
+    end
+    
+    local index = tonumber(args[1])
+    
+    if not index then
+        Msg("Invalid index\n")
+    end
+    
+    index = math.floor(index)
+    
+    if index < 1 or index > #measurePoints + 1 then
+        Msg("Invalid index\n")
+        return
+    end
+    
+    table.insert(measurePoints, index, vec)
+    print("Inserted point at #" .. index)
+    
+    RecalcMeasuredTotal()
+    Rehook()
+end
+
+--- Console command to replace a point in the path measurement tool.
+-- @param ply Player
+-- @param cmd Command
+-- @param args Arguments
+local function ReplaceMeasuredPoint(ply, cmd, args)
+    local vec = nil
+    
+    if #args == 2 or #args == 4 then
+        vec = SaitoHUD.ParseConcmdVector(args, 1)
+    elseif #args ~= 1 then
+        Msg("Invalid number of arguments\n")
+        return
+    else
+        local tr = SaitoHUD.GetRefTrace()
+        vec = tr.HitPos
+    end
+    
+    if vec == nil then
+        Msg("Invalid arguments\n")
+        return
+    end
+    
+    local index = tonumber(args[1])
+    
+    if not measurePoints[index] then
+        Msg("No such index\n")
+        return
+    end
+    
+    measurePoints[index] = vec
+    print("Replaced point #" .. index)
+    
+    RecalcMeasuredTotal()
+    Rehook()
+end
+
+--- Console command to remove a point in the path measurement tool.
+-- @param ply Player
+-- @param cmd Command
+-- @param args Arguments
+local function RemoveMeasuredPoint(ply, cmd, args)
+    local vec = nil
+    
+    if #args == 2 or #args == 4 then
+        vec = SaitoHUD.ParseConcmdVector(args, 1)
+    elseif #args ~= 1 then
+        Msg("Invalid number of arguments\n")
+        return
+    else
+        local tr = SaitoHUD.GetRefTrace()
+        vec = tr.HitPos
+    end
+    
+    if vec == nil then
+        Msg("Invalid arguments\n")
+        return
+    end
+    
+    local index = tonumber(args[1])
+    
+    if not measurePoints[index] then
+        Msg("No such index\n")
+        return
+    end
+    
+    table.remove(measurePoints, index)
+    print("Removed point #" .. index)
+    
+    RecalcMeasuredTotal()
+    Rehook()
+end
+
+--- Console command to remove the last point in the path measurement tool.
+-- @param ply Player
+-- @param cmd Command
+-- @param args Arguments
+local function RemoveLastMeasuredPoint(ply, cmd, args)
+    RemoveMeasuredPoint(ply, cmd, {#measurePoints})
+end
+
+--- Console command to list points added in the measurement tool.
+-- @param ply Player
+-- @param cmd Command
+-- @param args Arguments
+local function ListMeasuredPoints(ply, cmd, args)
+    if #args ~= 0 then
+        Msg("Invalid number of arguments\n")
+        return
+    end
+    
+    if #measurePoints > 0 then
+        for k, pt in pairs(measurePoints) do
+            if k == 1 then
+                print(string.format("#%d (%s)",k, tostring(pt)))
+            else
+                print(string.format("#%d (%s) incr. dist.: %f",
+                                    k, tostring(pt), pt:Distance(measurePoints[k - 1])))
+            end
+        end
+        
+        print(string.format("Total distance: %f", measureTotalLength))
+    else
+        print("No points!")
+    end
+end
+
+--- Console command to sum point distances in the measurement tool.
+-- @param ply Player
+-- @param cmd Command
+-- @param args Arguments
+local function SumMeasuredPoints(ply, cmd, args)
+    if #args ~= 2 then
+        Msg("Invalid number of arguments\n")
+        return
+    end
+    
+    local index1 = tonumber(args[1])
+    local index2 = tonumber(args[2])
+    
+    if not index1 or not index2 or index1 >= index2 then
+        Msg("Invalid arguments\n")
+        return
+    end
+    
+    index1 = math.floor(index1)
+    index2 = math.floor(index2)
+    
+    if index1 < 1 or index2 > #measurePoints then
+        Msg("Indexes out of range\n")
+        return
+    end
+    
+    local last = measurePoints[index1]
+    local total = 0
+    
+    for i = index1 + 1, index2 do
+        local pt = measurePoints[i]
+        total = total + pt:Distance(last)
+        last = pt
+    end
+    
+    print(string.format("Total distance from #%d -> #%d: %f", index1, index2, total))
+end
+
+--- Console command to get the distance between two points
+-- @param ply Player
+-- @param cmd Command
+-- @param args Arguments
+local function BetweenMeasuredPoints(ply, cmd, args)
+    if #args ~= 2 then
+        Msg("Invalid number of arguments\n")
+        return
+    end
+    
+    local index1 = tonumber(args[1])
+    local index2 = tonumber(args[2])
+    
+    if not index1 or not index2 or index1 >= index2 then
+        Msg("Invalid arguments\n")
+        return
+    end
+    
+    index1 = math.floor(index1)
+    index2 = math.floor(index2)
+    
+    if index1 == index2 then
+        Msg("Both indexes are the same\n")
+        return
+    end
+    
+    local distance = measurePoints[index1]:Distance(measurePoints[index2])
+    
+    print(string.format("Direct distance between #%d and #%d: %f", index1, index2, distance))
+end
+
+--- Console commands to clear the list of points.
+-- @param ply Player
+-- @param cmd Command
+-- @param args Arguments
+local function ClearMeasuredPoints(ply, cmd, args)
+    if #args ~= 0 then
+        Msg("Invalid number of arguments\n")
+        return
+    end
+    
+    measurePoints = {}
+    print("Cleared")
+end
+
 --- Console commands clear the list of reflection traces.
 -- @param ply Player
 -- @param cmd Command
@@ -123,6 +408,21 @@ local function DoDrawSurveyScreenspace()
                                      255 * (1 - k / #lines), 255)
             end
             SaitoHUD.Draw3D2DLine(v[1], v[2])
+        end
+    end
+    
+    -- Since the lines are long, we cannot draw on the HUD because lines with
+    -- end points that are off screen may not appear right
+    surface.SetDrawColor(255, 0, 255, 255)
+    if #measurePoints > 1 then
+        local last = measurePoints[1]
+        
+        for i = 2, #measurePoints do
+            local pt = measurePoints[i]
+            
+            SaitoHUD.Draw3D2DLine(last, pt)
+            
+            last = pt
         end
     end
 end
@@ -166,6 +466,65 @@ local function DrawReflectAnalysisText()
     end
 end
 
+--- Draw measured path.
+local function DrawMeasuringLines()
+    local dim = 5
+    surface.SetDrawColor(255, 0, 255, 255)
+    
+    if #measurePoints > 1 then
+        local last = measurePoints[1]
+        local lastScreen = last:ToScreen()
+        
+        if lastScreen.visible then
+            draw.SimpleText(tostring(1),
+                            "DefaultSmallDropShadow", lastScreen.x, lastScreen.y,
+                            Color(255, 255, 255, 255), 1, ALIGN_TOP)
+        end
+        
+        for i = 2, #measurePoints do
+            local pt = measurePoints[i]
+            local midPt = (pt - last) / 2 + last
+            local ptScreen = pt:ToScreen()
+            local midPtScreen = midPt:ToScreen()
+            
+            if ptScreen.visible and lastScreen.visible then
+                surface.DrawLine(lastScreen.x, lastScreen.y, ptScreen.x, ptScreen.y)
+            end
+            
+            if midPtScreen.visible then
+                draw.SimpleText(string.format("%0.2f", last:Distance(pt)),
+                                "DefaultSmallDropShadow",
+                                midPtScreen.x, midPtScreen.y,
+                                Color(255, 200, 255, 255), 1, ALIGN_TOP)
+            end
+            
+            if ptScreen.visible then
+                draw.SimpleText(tostring(i), "DefaultSmallDropShadow",
+                                ptScreen.x - 2, ptScreen.y - 5,
+                                Color(255, 255, 255, 255), 1, ALIGN_TOP)
+            end
+            
+            last = pt
+            lastScreen = last:ToScreen()
+        end
+
+        local yOffset = ScrH() * 0.3 - 50
+        local color = Color(255, 200, 255, 255)
+        draw.SimpleText("Measured Total: " .. measureTotalLength,
+                        "TabLarge", ScrW() - 16, yOffset, color, 2, ALIGN_TOP)
+    elseif #measurePoints == 1 then
+        local dim = 5
+        local last = measurePoints[1]
+        local lastScreen = last:ToScreen()
+        surface.DrawOutlinedRect(lastScreen.x - dim / 2,
+                                 lastScreen.y - dim / 2,
+                                 dim, dim)
+        draw.SimpleText(tostring(1),
+                        "DefaultSmallDropShadow", lastScreen.x, lastScreen.y,
+                        Color(255, 255, 255, 255), 1, ALIGN_TOP)
+    end
+end
+
 --- Hook to draw survey stuff in RenderScreenspaceEffects.
 local function DrawSurveyScreenspace()
     cam.Start3D(EyePos(), EyeAngles())
@@ -184,10 +543,12 @@ local function DrawSurvey()
     if reflectTraceNodes:GetBool() then
         DrawReflectAnalysisText()
     end
+    
+    DrawMeasuringLines()
 end
 
 Rehook = function()
-    if #orthogonalTraces > 0 or #reflectionLines > 0 then
+    if #orthogonalTraces > 0 or #reflectionLines > 0 or #measurePoints > 0 then
         hook.Add("RenderScreenspaceEffects", "SaitoHUD.Survey", DrawSurveyScreenspace)
         hook.Add("HUDPaint", "SaitoHUD.Survey", DrawSurvey)
     else
@@ -202,3 +563,12 @@ concommand.Add("ortho_trace", OrthoTrace)
 concommand.Add("ortho_trace_clear", OrthoTraceClear)
 concommand.Add("reflect_trace", ReflectAnalysis)
 concommand.Add("reflect_trace_clear", ReflectAnalysisClear)
+concommand.Add("measure_add", AddMeasuredPoint)
+concommand.Add("measure_insert", InsertMeasuredPoint)
+concommand.Add("measure_list", ListMeasuredPoints)
+concommand.Add("measure_clear", ClearMeasuredPoints)
+concommand.Add("measure_sum", SumMeasuredPoints)
+concommand.Add("measure_between", BetweenMeasuredPoints)
+concommand.Add("measure_remove", RemoveMeasuredPoint)
+concommand.Add("measure_remove_last", RemoveLastMeasuredPoint)
+concommand.Add("measure_replace", ReplaceMeasuredPoint)
